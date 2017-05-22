29a18626275d652149694e36f23a9e0a633a39e3
gartenriese2 <gartenriese@autistici.org>
2017-05-22 14:55:08 +0200

    added active connections

--- TransitCity/Transit/Data/TransitConnectionInfo.cs
+++ TransitCity/Transit/Data/TransitConnectionInfo.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using CitySimulation;
+using Time;
 using Transit.Timetable;
 
 namespace Transit.Data
@@ -32,6 +34,30 @@ namespace Transit.Data
         public float GetPercentageOfWorkersUsingTransitOnly()
         {
             return _connectionsDictionary.Select(d => d.Value).Count(l => l.All(c => c.Count > 1)) * 100f / _connectionsDictionary.Count;
+        }
+
+        public IEnumerable<Connection> GetActiveConnections(WeekTimePoint wtp)
+        {
+            return _connectionsDictionary.Values.SelectMany(cll => cll)
+                                                .SelectMany(cl => cl)
+                                                .Where(c => new WeekTimeSpan(c.SourceTime, c.TargetTime).IsInside(wtp));
+        }
+
+        public IEnumerable<Connection> GetActiveConnectionsThreaded(WeekTimePoint wtp)
+        {
+            var numThreads = Environment.ProcessorCount;
+            if (numThreads <= 1)
+            {
+                return GetActiveConnections(wtp);
+            }
+
+            var taskList = new List<Task<List<Connection>>>(numThreads);
+            var connections = _connectionsDictionary.Values.SelectMany(cll => cll).SelectMany(cl => cl);
+            var i = 0;
+            var groups = connections.GroupBy(x => i++ % numThreads).ToList();
+            taskList.AddRange(groups.Select(g => Task.Factory.StartNew(() => g.Where(c => new WeekTimeSpan(c.SourceTime, c.TargetTime).IsInside(wtp)).ToList())));
+            Task.WaitAll(taskList.ToArray());
+            return taskList.Select(t => t.Result).SelectMany(r => r);
         }
 
         private void Initialize()
--- TransitCity/WpfTestApp/MainWindowViewModel.cs
+++ TransitCity/WpfTestApp/MainWindowViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
+using CitySimulation;
+using Geometry.Shapes;
 using Time;
 using Transit.Data;
+using Transit.Timetable;
+using Transit.Timetable.Algorithm;
 using Utility.MVVM;
+using Utility.Units;
 using WpfDrawing.Objects;
 using WpfDrawing.Panel;
 using WpfDrawing.Utility;
@@ -17,6 +24,7 @@ namespace WpfTestApp
     public class MainWindowViewModel : PropertyChangedBase
     {
         private readonly DataManager _dataManager;
+        private readonly TransitConnectionInfo _transitConnectionInfo;
         private TimeSpan _time = TimeSpan.Zero;
         private string _weektime = string.Empty;
         private double _timeDelta = 5.0;
@@ -25,10 +33,31 @@ namespace WpfTestApp
         private double _centerX;
         private double _centerY;
         private double _zoom;
+        private int _activeConnectionsCount;
 
         public MainWindowViewModel()
         {
+            var city = CreateCity();
+            var rnd = new Random();
+            var workerScheduleTuples = city.Residents.Where(r => r.HasJob).Select(r => (r, JobSchedule.CreateRandom(rnd))).ToList();
             _dataManager = new TestTransitData().DataManager;
+            var raptor = new Raptor(TimeSpan.FromMinutes(10), TimeSpan.FromMinutes(15), _dataManager);
+            var workerConnectionsDictionary = new Dictionary<Resident, List<List<Connection>>>();
+            foreach (var (worker, schedule) in workerScheduleTuples)
+            {
+                var workerTaskList = new List<Task<List<Connection>>>();
+                foreach (var scheduleWts in schedule.WeekTimeSpans)
+                {
+                    workerTaskList.Add(Task.Factory.StartNew(() => raptor.ComputeReverse(worker.Position, scheduleWts.Begin, worker.Job.Position, Speed.FromKilometersPerHour(8))));
+                    workerTaskList.Add(Task.Factory.StartNew(() => raptor.Compute(worker.Job.Position, scheduleWts.End, worker.Position, Speed.FromKilometersPerHour(8))));
+                }
+
+                Task.WaitAll(workerTaskList.ToArray());
+                workerConnectionsDictionary.Add(worker, workerTaskList.Select(t => t.Result).ToList());
+            }
+
+            _transitConnectionInfo = new TransitConnectionInfo(workerConnectionsDictionary);
+
             PlusCommand = new RelayCommand(o => _timeDelta *= 2.0, o => _timeDelta < 20.0);
             MinusCommand = new RelayCommand(o => _timeDelta /= 2.0, o => _timeDelta > 0.001);
             CenterX = 0.5;
@@ -167,6 +196,19 @@ namespace WpfTestApp
             }
         }
 
+        public int ActiveConnectionsCount
+        {
+            get => _activeConnectionsCount;
+            set
+            {
+                if (value != _activeConnectionsCount)
+                {
+                    _activeConnectionsCount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         public double MinZoom { get; } = 0.1;
 
         public double MaxZoom { get; } = 50.0;
@@ -200,6 +242,199 @@ namespace WpfTestApp
             }
         }
 
+        private static City CreateCity()
+        {
+            var balham = new RandomDistrict("Balham", new Polygon(
+                5000, 4500,
+                5250, 4550,
+                5500, 4625,
+                5750, 4675,
+                6000, 4700,
+                6250, 4610,
+                6500, 4500,
+                6750, 4340,
+                7000, 4150,
+                7000, 4500,
+                6500, 5500,
+                5500, 6000,
+                5000, 5500
+            ), 4500, 16000);
+
+            var buckhurst = new RandomDistrict("Buckhurst", new Polygon(
+                2500, 5400,
+                2750, 5450,
+                3000, 5410,
+                3250, 5350,
+                3500, 5260,
+                3750, 5125,
+                4000, 5000,
+                4250, 4850,
+                4500, 4670,
+                4750, 4570,
+                5000, 4500,
+                5000, 5500,
+                5500, 6000,
+                4500, 6500,
+                2500, 6500
+            ), 8000, 3000);
+
+            var epping = new RandomDistrict("Epping", new Polygon(
+                0, 3600,
+                250, 3620,
+                500, 3660,
+                750, 3750,
+                1000, 4000,
+                1850, 5000,
+                2000, 5120,
+                2250, 5270,
+                2500, 5400,
+                2500, 6500,
+                1000, 7000,
+                0, 8000
+            ), 6000, 2000);
+
+            var morden = new RandomDistrict("Morden", new Polygon(
+                0, 10000,
+                0, 8000,
+                1000, 7000,
+                2500, 6500,
+                3000, 6500,
+                3000, 8000,
+                4500, 10000
+            ), 10000, 5000);
+
+            var neasden = new RandomDistrict("Neasden", new Polygon(
+                4500, 3500,
+                5000, 3000,
+                6000, 2500,
+                6000, 4270,
+                5750, 4300,
+                5500, 4260,
+                5250, 4150,
+                5000, 4120,
+                4750, 4160,
+                4500, 4250
+            ), 5000, 14000);
+
+            var clapham = new RandomDistrict("Clapham", new Polygon(
+                6000, 4270,
+                6000, 1500,
+                7000, 1000,
+                7500, 1500,
+                7500, 2740,
+                7250, 3010,
+                7000, 3400,
+                6750, 3840,
+                6500, 4050,
+                6250, 4170
+            ), 12000, 6000);
+
+            var stockwell = new RandomDistrict("Stockwell", new Polygon(
+                7500, 1500,
+                7000, 1000,
+                7500, 0,
+                10000, 0,
+                10000, 2300,
+                9750, 2200,
+                9500, 2100,
+                9250, 2060,
+                9000, 2040,
+                8750, 2080,
+                8500, 2150,
+                8250, 2250,
+                8000, 2390,
+                7750, 2550,
+                7500, 2740
+            ), 2000, 5000);
+
+            var debden = new RandomDistrict("Debden", new Polygon(
+                0, 3000,
+                5000, 3000,
+                4500, 3500,
+                4500, 4250,
+                3500, 4750,
+                3000, 4950,
+                2750, 5000,
+                2500, 4950,
+                2250, 4800,
+                2000, 4630,
+                1500, 4000,
+                1250, 3700,
+                1000, 3490,
+                750, 3350,
+                500, 3270,
+                0, 3270
+            ), 8000, 3000);
+
+            var amersham = new RandomDistrict("Amersham", new Polygon(
+                3000, 3000,
+                4000, 1500,
+                6000, 1500,
+                6000, 2500,
+                5000, 3000
+            ), 7500, 4000);
+
+            var chesham = new RandomDistrict("Chesham", new Polygon(
+                4000, 1500,
+                4000, 0,
+                7500, 0,
+                7000, 1000,
+                6000, 1500
+            ), 3000, 2000);
+
+            var stanmore = new RandomDistrict("Stanmore", new Polygon(
+                0, 3000,
+                0, 0,
+                4000, 0,
+                4000, 1500,
+                3000, 3000
+            ), 4500, 1500);
+
+            var tooting = new RandomDistrict("Tooting", new Polygon(
+                3000, 6500,
+                4500, 6500,
+                6500, 5500,
+                7500, 7500,
+                8000, 10000,
+                4500, 10000,
+                3000, 8000
+            ), 3000, 500);
+
+            var hampstead = new RandomDistrict("Hampstead", new Polygon(
+                7000, 6500,
+                10000, 6500,
+                10000, 10000,
+                8000, 10000,
+                7500, 7500
+            ), 4000, 1000);
+
+            var watfrord = new RandomDistrict("Watford", new Polygon(
+                7000, 4500,
+                10000, 4500,
+                10000, 6500,
+                7000, 6500,
+                6500, 5500
+            ), 8000, 4000);
+
+            var chigwell = new RandomDistrict("Chigwell", new Polygon(
+                7000, 4500,
+                7000, 4150,
+                7150, 4000,
+                7500, 3500,
+                7750, 3200,
+                8000, 3000,
+                8500, 2720,
+                8750, 2640,
+                9000, 2600,
+                9500, 2660,
+                9750, 2750,
+                10000, 2900,
+                10000, 4500
+            ), 7000, 2500);
+
+            return new City("London", new List<IDistrict> { balham, buckhurst, epping, morden, neasden, clapham, stockwell, debden, amersham, chesham, stanmore, tooting, hampstead, watfrord, chigwell });
+        }
+
         private void OnTimerTick2(object sender, EventArgs args)
         {
             var sw = new Stopwatch();
@@ -222,6 +457,10 @@ namespace WpfTestApp
                 var v = new Vehicle(activeVehicle.Item1, activeVehicle.Item2.Normalize());
                 PanelObjects.Add(v);
             }
+
+            var activeConnections = _transitConnectionInfo.GetActiveConnections(wtp);
+            ActiveConnectionsCount = activeConnections.Count();
+
             sw.Stop();
             SimulationTime = sw.ElapsedMilliseconds;
         }
