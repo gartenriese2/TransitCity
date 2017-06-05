c1c1381e09448b36f0f19c514b179d3c5f298b8b
gartenriese2 <bastian.mattes@informatik.stud.uni-erlangen.de>
2017-06-05 16:32:16 +0200

    more work on departurescontrol

--- TransitCity/WpfDrawing/Timetable/DepartureData.cs
+++ TransitCity/WpfDrawing/Timetable/DepartureData.cs
@@ -1,17 +1,53 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WpfDrawing.Timetable
 {
     public class DepartureData
     {
-        public int Hour { get; set; }
+        public DepartureData(int hour, List<int> weekday, List<int> friday, List<int> saturday, List<int> sunday)
+        {
+            if (hour < 0 || hour > 23)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hour));
+            }
 
-        public List<int> MinutesWeekday { get; } = new List<int>();
+            if (weekday.Any(t => t < 0 || t > 59))
+            {
+                throw new ArgumentOutOfRangeException(nameof(weekday));
+            }
 
-        public List<int> MinutesFriday { get; } = new List<int>();
+            if (friday.Any(t => t < 0 || t > 59))
+            {
+                throw new ArgumentOutOfRangeException(nameof(friday));
+            }
 
-        public List<int> MinutesSaturday { get; } = new List<int>();
+            if (saturday.Any(t => t < 0 || t > 59))
+            {
+                throw new ArgumentOutOfRangeException(nameof(saturday));
+            }
 
-        public List<int> MinutesSunday { get; } = new List<int>();
+            if (sunday.Any(t => t < 0 || t > 59))
+            {
+                throw new ArgumentOutOfRangeException(nameof(sunday));
+            }
+
+            Hour = hour;
+            MinutesWeekday = weekday.OrderBy(t => t);
+            MinutesFriday = friday.OrderBy(t => t);
+            MinutesSaturday = saturday.OrderBy(t => t);
+            MinutesSunday = sunday.OrderBy(t => t);
+        }
+
+        public int Hour { get; }
+
+        public IEnumerable<int> MinutesWeekday { get; }
+
+        public IEnumerable<int> MinutesFriday { get; }
+
+        public IEnumerable<int> MinutesSaturday { get; }
+
+        public IEnumerable<int> MinutesSunday { get; }
     }
 }
--- TransitCity/WpfDrawing/Timetable/DeparturesInfoViewModel.cs
+++ TransitCity/WpfDrawing/Timetable/DeparturesInfoViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Transit.Data;
 using Utility.MVVM;
@@ -14,27 +15,30 @@ namespace WpfDrawing.Timetable
                 throw new InvalidOperationException();
             }
 
-            DepartureInfoCollection.Add(new DepartureData
-            {
-                Hour = 0,
-                MinutesWeekday = { 8, 18, 28, 38, 48, 58 },
-                MinutesFriday = { 8, 28, 48 },
-                MinutesSaturday = { 8, 38 }
-            });
-            DepartureInfoCollection.Add(new DepartureData
-            {
-                Hour = 1,
-                MinutesWeekday = { 8, 18, 28, 38, 48, 58 },
-                MinutesFriday = { 8, 28, 48 },
-                MinutesSaturday = { 8, 38 }
-            });
-            DepartureInfoCollection.Add(new DepartureData
-            {
-                Hour = 2,
-                MinutesWeekday = { 8, 18, 28, 38, 48, 58 },
-                MinutesFriday = { 8, 28, 48 },
-                MinutesSaturday = { 8, 38 }
-            });
+            DepartureInfoCollection.Add(new DepartureData(0, new List<int>{ 9, 19, 29, 39, 47 }, new List<int>{ 9, 19, 29, 39, 47 }, new List<int>{ 9, 19, 29, 39, 47 }, new List<int>{ 9, 19, 29, 39, 47 }));
+            DepartureInfoCollection.Add(new DepartureData(1, new List<int>(), new List<int>(), new List<int>(), new List<int>()));
+            DepartureInfoCollection.Add(new DepartureData(2, new List<int>(), new List<int>(), new List<int>(), new List<int>()));
+            DepartureInfoCollection.Add(new DepartureData(3, new List<int>(), new List<int>(), new List<int>(), new List<int>()));
+            DepartureInfoCollection.Add(new DepartureData(4, new List<int> { 51, 58 }, new List<int> { 51, 58 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(5, new List<int> { 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(6, new List<int> { 0, 6, 13, 20, 26, 33, 39, 44, 49, 54, 59 }, new List<int> { 0, 6, 13, 20, 26, 33, 39, 44, 49, 54, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(7, new List<int> { 4, 9 , 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(8, new List<int> { 4, 9, 14, 19, 23, 26, 33, 39, 46, 53, 59 }, new List<int> { 4, 9, 14, 19, 23, 26, 33, 39, 46, 53, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(9, new List<int> { 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(10, new List<int> { 0, 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 0, 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(11, new List<int> { 0, 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 0, 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(12, new List<int> { 0, 6, 13, 20, 26, 33, 39, 44, 49, 54, 59 }, new List<int> { 0, 6, 13, 20, 26, 33, 39, 44, 49, 54, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(13, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(14, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(15, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(16, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(17, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(18, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(19, new List<int> { 2, 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 2, 6, 13, 20, 26, 33, 40, 46, 53 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(20, new List<int> { 0, 6, 13, 20, 26, 33, 40, 49, 59 }, new List<int> { 0, 6, 13, 20, 26, 33, 40, 49, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(21, new List<int> { 9, 19, 29, 39, 49, 59 }, new List<int> { 9, 19, 29, 39, 49, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(22, new List<int> { 9, 19, 29, 39, 49, 59 }, new List<int> { 9, 19, 29, 39, 49, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
+            DepartureInfoCollection.Add(new DepartureData(23, new List<int> { 9, 19, 29, 39, 49, 59 }, new List<int> { 9, 19, 29, 39, 49, 59 }, new List<int> { 8 }, new List<int> { 8, 48 }));
         }
 
         public DeparturesInfoViewModel(StationInfo stationInfo)
