ac9e4815a11ae62fdad3731d4ba530fa06f3783d
gartenriese2 <bastian.mattes@informatik.stud.uni-erlangen.de>
2017-06-06 21:30:58 +0200

    added small london & transfers

--- TransitCity/WpfTestApp/MainWindowViewModel.cs
+++ TransitCity/WpfTestApp/MainWindowViewModel.cs
@@ -455,6 +455,199 @@ namespace WpfTestApp
             return new City("London", new List<IDistrict> { balham, buckhurst, epping, morden, neasden, clapham, stockwell, debden, amersham, chesham, stanmore, tooting, hampstead, watfrord, chigwell });
         }
 
+        private static City CreateSmallLondon()
+        {
+            var balham = new RandomDistrict("Balham", new Polygon(
+                5000, 4500,
+                5250, 4550,
+                5500, 4625,
+                5750, 4675,
+                6000, 4700,
+                6250, 4610,
+                6500, 4500,
+                6750, 4340,
+                7000, 4150,
+                7000, 4500,
+                6500, 5500,
+                5500, 6000,
+                5000, 5500
+            ), 450, 1600);
+
+            var buckhurst = new RandomDistrict("Buckhurst", new Polygon(
+                2500, 5400,
+                2750, 5450,
+                3000, 5410,
+                3250, 5350,
+                3500, 5260,
+                3750, 5125,
+                4000, 5000,
+                4250, 4850,
+                4500, 4670,
+                4750, 4570,
+                5000, 4500,
+                5000, 5500,
+                5500, 6000,
+                4500, 6500,
+                2500, 6500
+            ), 800, 300);
+
+            var epping = new RandomDistrict("Epping", new Polygon(
+                0, 3600,
+                250, 3620,
+                500, 3660,
+                750, 3750,
+                1000, 4000,
+                1850, 5000,
+                2000, 5120,
+                2250, 5270,
+                2500, 5400,
+                2500, 6500,
+                1000, 7000,
+                0, 8000
+            ), 600, 200);
+
+            var morden = new RandomDistrict("Morden", new Polygon(
+                0, 10000,
+                0, 8000,
+                1000, 7000,
+                2500, 6500,
+                3000, 6500,
+                3000, 8000,
+                4500, 10000
+            ), 1000, 500);
+
+            var neasden = new RandomDistrict("Neasden", new Polygon(
+                4500, 3500,
+                5000, 3000,
+                6000, 2500,
+                6000, 4270,
+                5750, 4300,
+                5500, 4260,
+                5250, 4150,
+                5000, 4120,
+                4750, 4160,
+                4500, 4250
+            ), 500, 1400);
+
+            var clapham = new RandomDistrict("Clapham", new Polygon(
+                6000, 4270,
+                6000, 1500,
+                7000, 1000,
+                7500, 1500,
+                7500, 2740,
+                7250, 3010,
+                7000, 3400,
+                6750, 3840,
+                6500, 4050,
+                6250, 4170
+            ), 1200, 600);
+
+            var stockwell = new RandomDistrict("Stockwell", new Polygon(
+                7500, 1500,
+                7000, 1000,
+                7500, 0,
+                10000, 0,
+                10000, 2300,
+                9750, 2200,
+                9500, 2100,
+                9250, 2060,
+                9000, 2040,
+                8750, 2080,
+                8500, 2150,
+                8250, 2250,
+                8000, 2390,
+                7750, 2550,
+                7500, 2740
+            ), 200, 500);
+
+            var debden = new RandomDistrict("Debden", new Polygon(
+                0, 3000,
+                5000, 3000,
+                4500, 3500,
+                4500, 4250,
+                3500, 4750,
+                3000, 4950,
+                2750, 5000,
+                2500, 4950,
+                2250, 4800,
+                2000, 4630,
+                1500, 4000,
+                1250, 3700,
+                1000, 3490,
+                750, 3350,
+                500, 3270,
+                0, 3270
+            ), 800, 300);
+
+            var amersham = new RandomDistrict("Amersham", new Polygon(
+                3000, 3000,
+                4000, 1500,
+                6000, 1500,
+                6000, 2500,
+                5000, 3000
+            ), 750, 400);
+
+            var chesham = new RandomDistrict("Chesham", new Polygon(
+                4000, 1500,
+                4000, 0,
+                7500, 0,
+                7000, 1000,
+                6000, 1500
+            ), 300, 200);
+
+            var stanmore = new RandomDistrict("Stanmore", new Polygon(
+                0, 3000,
+                0, 0,
+                4000, 0,
+                4000, 1500,
+                3000, 3000
+            ), 450, 150);
+
+            var tooting = new RandomDistrict("Tooting", new Polygon(
+                3000, 6500,
+                4500, 6500,
+                6500, 5500,
+                7500, 7500,
+                8000, 10000,
+                4500, 10000,
+                3000, 8000
+            ), 300, 50);
+
+            var hampstead = new RandomDistrict("Hampstead", new Polygon(
+                7000, 6500,
+                10000, 6500,
+                10000, 10000,
+                8000, 10000,
+                7500, 7500
+            ), 400, 100);
+
+            var watfrord = new RandomDistrict("Watford", new Polygon(
+                7000, 4500,
+                10000, 4500,
+                10000, 6500,
+                7000, 6500,
+                6500, 5500
+            ), 800, 400);
+
+            var chigwell = new RandomDistrict("Chigwell", new Polygon(
+                7000, 4500,
+                7000, 4150,
+                7150, 4000,
+                7500, 3500,
+                7750, 3200,
+                8000, 3000,
+                8500, 2720,
+                8750, 2640,
+                9000, 2600,
+                9500, 2660,
+                9750, 2750,
+                10000, 2900,
+                10000, 4500
+            ), 700, 250);
+
+            return new City("London", new List<IDistrict> { balham, buckhurst, epping, morden, neasden, clapham, stockwell, debden, amersham, chesham, stanmore, tooting, hampstead, watfrord, chigwell });
+        }
+
         private static City CreateSmallCity()
         {
             var district = new RandomDistrict("City", new Polygon(
@@ -492,18 +685,32 @@ namespace WpfTestApp
 
             var activeConnections = _transitConnectionInfo.GetActiveConnections(wtp).ToList();
             ActiveConnectionsCount = activeConnections.Count;
-            var walkingConnections = activeConnections.Where(c => c.Type == Connection.TypeEnum.WalkToStation || c.Type == Connection.TypeEnum.WalkFromStation);
+            var walkingConnections = activeConnections.Where(c => c.Type == Connection.TypeEnum.WalkToStation || c.Type == Connection.TypeEnum.WalkFromStation || c.Type == Connection.TypeEnum.Transfer);
             var residents = new List<ResidentObject>();
             foreach (var wc in walkingConnections)
             {
-                var toStation = wc.Type == Connection.TypeEnum.WalkToStation;
-                var vec = toStation ? wc.TargetStation.EntryPosition - wc.SourcePos : wc.TargetPos - wc.SourceStation.ExitPosition;
-                var from = toStation ? wc.SourcePos : wc.SourceStation.ExitPosition;
-                var to = toStation ? wc.TargetStation.EntryPosition : wc.TargetPos;
-                var t = (wtp - wc.SourceTime).TotalMilliseconds / (wc.TargetTime - wc.SourceTime).TotalMilliseconds;
-                var pos = Position2d.Lerp(t, from, to);
-                var r = new ResidentObject(pos, vec);
-                residents.Add(r);
+                var isTransfer = wc.Type == Connection.TypeEnum.Transfer;
+                if (isTransfer)
+                {
+                    var vec = wc.TargetStation.EntryPosition - wc.SourceStation.ExitPosition;
+                    var from = wc.SourceStation.ExitPosition;
+                    var to = wc.TargetStation.EntryPosition;
+                    var t = (wtp - wc.SourceTime).TotalMilliseconds / (wc.TargetTime - wc.SourceTime).TotalMilliseconds;
+                    var pos = Position2d.Lerp(t, from, to);
+                    var r = new ResidentObject(pos, vec);
+                    residents.Add(r);
+                }
+                else
+                {
+                    var toStation = wc.Type == Connection.TypeEnum.WalkToStation;
+                    var vec = toStation ? wc.TargetStation.EntryPosition - wc.SourcePos : wc.TargetPos - wc.SourceStation.ExitPosition;
+                    var from = toStation ? wc.SourcePos : wc.SourceStation.ExitPosition;
+                    var to = toStation ? wc.TargetStation.EntryPosition : wc.TargetPos;
+                    var t = (wtp - wc.SourceTime).TotalMilliseconds / (wc.TargetTime - wc.SourceTime).TotalMilliseconds;
+                    var pos = Position2d.Lerp(t, from, to);
+                    var r = new ResidentObject(pos, vec);
+                    residents.Add(r);
+                }
             }
 
             residents.ForEach(r => PanelObjects.Add(r));
@@ -516,7 +723,7 @@ namespace WpfTestApp
         {
             PercentageLoadedVisibility = Visibility.Visible;
 
-            var city = CreateSmallCity();
+            var city = CreateSmallLondon();
             var rnd = new Random();
             var workerScheduleTuples = city.Residents.Where(r => r.HasJob).Select(r => (r, JobSchedule.CreateRandom(rnd))).ToList();
             var raptor = new Raptor(TimeSpan.FromMinutes(10), TimeSpan.FromMinutes(15), _dataManager);
